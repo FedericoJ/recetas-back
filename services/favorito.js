dfe0802d0a8a8a41281b070c5779f45025bbfe2a
unknown <german.mainero@outlook.com>
2022-04-25 19:31:19 -0300

    agregar editar y ver favoritos

---
+++ services/favorito.js
@@ -0,0 +1,81 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const env = process.env.NODE_ENV || 'development';
+const configClave = require('../config/config.json')[env]
+
+
+async function getFavoritos(usuario){
+
+  try{
+    const rows = await db.query(
+      `select c.calificacion, r.nombre, u.nickname from favoritos f
+      join calificaciones c on f.idreceta = c.idreceta
+      join recetas r on f.idreceta = r.idreceta
+      join usuarios u on r.idusuario = u.idusuario 
+      where f.idusuario ='${usuario.idUsuario}'`
+    );
+    const data = helper.emptyOrRows(rows);
+    
+    return {code: 201, favorito:data};
+
+  }catch(e){
+
+    return {code: 400, message: e.message};
+  }
+  
+
+}
+
+async function cargarFavorito(receta,usuario){
+
+  try{
+    const rows = await db.query(
+      `insert into favoritos
+      (idReceta,idUsuario)
+      VALUES
+      (${receta.idReceta},${usuario.idUsuario});`
+    );
+
+    let message = 'Error al guardar en favorito';
+    
+      if (rows.affectedRows) {
+        message = 'Favorito guardado correctamente';
+      }
+    
+      return {code: 201, message:message};
+
+  }catch(e){
+    return {code: 400, message:e.message};
+  }
+  
+}
+
+async function eliminarFavorito(usuario,receta){
+
+  try{
+    const rows = await db.query(
+      `delete from favoritos 
+      where idUsuario ='${usuario.idUsuario}'
+      and idReceta ='${receta.idReceta}' `
+    );
+    const data = helper.emptyOrRows(rows);
+    
+    return {code: 201, favorito:data};
+
+  }catch(e){
+
+    return {code: 400, message: e.message};
+  }
+  
+
+}
+
+
+module.exports = {
+  getFavoritos,
+  cargarFavorito,
+  eliminarFavorito
+}
