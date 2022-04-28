545c2ad16f5a39c9f591547b9d29900d5daff1b7
fede <17federico@gmail.com>
2022-04-28 20:45:37 -0300

    Arreglo PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||  3000;
 const usuarioRouter = require("./routes/usuario");
 const ingredienteRouter = require('./routes/ingredientes');
 const recetaRouter = require('./routes/receta');
